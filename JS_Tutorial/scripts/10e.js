3ee06d41186393d535601737adf3530e8137a4a6
ZanZubair96 <zanzubair2@gmail.com>
2024-07-15 13:03:44 +0530

    toggle button

--- JS_Tutorial/scripts/10e.js
+++ JS_Tutorial/scripts/10e.js
@@ -1,26 +1,17 @@
-function gamingToggles(){
-  if(document.querySelector('.gaming-button').classList.contains('is-toggled')){
-    document.querySelector('.gaming-button').classList.remove('is-toggled');
-  }else {
-    document.querySelector('.gaming-button').classList.add('is-toggled');
-  }
-  
+function toggleButton(selector){
+  const button = document.querySelector(selector);
+  if(!button.classList.contains('is-toggled')){
+    turnOffPreviousButton();
+    button.classList.add('is-toggled');
+  }else{
+    button.classList.remove('is-toggled');
 }
 
-function musicToggles(){
-  if(document.querySelector('.music-button').classList.contains('is-toggled')){
-    document.querySelector('.music-button').classList.remove('is-toggled');
-  }else {
-    document.querySelector('.music-button').classList.add('is-toggled');
+function turnOffPreviousButton(){
+  const previousButton = document.querySelector('.is-toggled');
+  if(previousButton){
+    previousButton.classList.remove('is-toggled');
   }
-  
 }
 
-function techToggles(){
-  if(document.querySelector('.tech-button').classList.contains('is-toggled')){
-    document.querySelector('.tech-button').classList.remove('is-toggled');
-  }else {
-    document.querySelector('.tech-button').classList.add('is-toggled');
-  }
-  
 }
